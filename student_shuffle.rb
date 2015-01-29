94e61a730425b4d0d00e3c8cfe84769c7906d913
Joverne Trotter <jojotrotter@gmail.com>
2015-01-29 09:37:48 -0500

    first commit

---
+++ student_shuffle.rb
@@ -0,0 +1,8 @@
+ #write a program that splits the class into groups of two.
+ # if there's an odd number of students, then the last group should be a group of 3.
+
+ students = ['Daniele', 'Aleisia', 'Anita', 'Ben', 'Thomas', 'Matthew', 'Mark', 'Jennifer', 'Cameron']
+
+puts students
+
+
